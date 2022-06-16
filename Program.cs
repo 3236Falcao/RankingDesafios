a0b5e12f57ccaeed08fa96f0148c70aee1a4c27b
Daniel Paulino da Silva Falcao <daniel.falcao@havan.com.br>
2022-06-16 00:36:25 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RankingDesafios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string respostaReal = "";
+
+            Console.WriteLine("Digite dois valores!");
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            string respostaIdeal = A + " + " + B + " = " + (A + B);
+
+            Console.WriteLine("Agora irei somar os valores");
+            respostaReal = A + " + " + B + " = " + (A + B);
+            Console.WriteLine(respostaReal);
+
+            if (respostaIdeal == respostaReal)
+            {
+                Console.WriteLine("Parabéns, você resolveu o dessafio!");
+            }
+            else
+            {
+                Console.WriteLine("Que pena, você não conseguiu resolver o desafio.");                
+            }
+        }
+    }
+}
